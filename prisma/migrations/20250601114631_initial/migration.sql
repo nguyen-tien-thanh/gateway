fa58fb9a8a936309505dc3b758d5230af179b540
Thanh Nguyen <nguyentienthanh.tgdd@gmail.com>
2025-06-01 18:47:00 +0700

    refactor: change postgres to mysql

---
+++ prisma/migrations/20250601114631_initial/migration.sql
@@ -0,0 +1,114 @@
+-- CreateTable
+CREATE TABLE `user` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `address` VARCHAR(191) NOT NULL,
+    `contact` VARCHAR(191) NOT NULL,
+    `avatar` VARCHAR(191) NOT NULL,
+    `status` ENUM('ACTIVE', 'INACTIVE', 'PENDING') NOT NULL,
+    `token` VARCHAR(191) NOT NULL,
+    `tokenValidityDate` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `salt` VARCHAR(191) NOT NULL,
+    `twoFASecret` VARCHAR(191) NULL,
+    `twoFAThrottleTime` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `isTwoFAEnabled` BOOLEAN NOT NULL DEFAULT false,
+    `roleId` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `user_username_key`(`username`),
+    UNIQUE INDEX `user_email_key`(`email`),
+    INDEX `user_username_idx`(`username`),
+    INDEX `user_email_idx`(`email`),
+    INDEX `user_name_idx`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `role` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `role_name_key`(`name`),
+    INDEX `role_name_idx`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `permission` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `resource` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `path` VARCHAR(191) NOT NULL,
+    `method` VARCHAR(191) NOT NULL DEFAULT 'GET',
+    `isDefault` BOOLEAN NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `permission_description_key`(`description`),
+    INDEX `permission_description_idx`(`description`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `role_permission` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `roleId` INTEGER NOT NULL,
+    `permissionId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `role_permission_roleId_permissionId_key`(`roleId`, `permissionId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `refresh_token` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` INTEGER NOT NULL,
+    `ip` VARCHAR(191) NOT NULL,
+    `userAgent` VARCHAR(191) NOT NULL,
+    `browser` VARCHAR(191) NULL,
+    `os` VARCHAR(191) NULL,
+    `isRevoked` BOOLEAN NOT NULL,
+    `expires` DATETIME(3) NOT NULL,
+
+    INDEX `refresh_token_browser_idx`(`browser`),
+    INDEX `refresh_token_os_idx`(`os`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `email_template` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `slug` VARCHAR(191) NOT NULL,
+    `sender` VARCHAR(191) NOT NULL,
+    `subject` VARCHAR(191) NOT NULL,
+    `body` TEXT NOT NULL,
+    `isDefault` BOOLEAN NOT NULL DEFAULT false,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `email_template_title_key`(`title`),
+    UNIQUE INDEX `email_template_slug_key`(`slug`),
+    INDEX `email_template_title_idx`(`title`),
+    INDEX `email_template_slug_idx`(`slug`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `user` ADD CONSTRAINT `user_roleId_fkey` FOREIGN KEY (`roleId`) REFERENCES `role`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `role_permission` ADD CONSTRAINT `role_permission_roleId_fkey` FOREIGN KEY (`roleId`) REFERENCES `role`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `role_permission` ADD CONSTRAINT `role_permission_permissionId_fkey` FOREIGN KEY (`permissionId`) REFERENCES `permission`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `refresh_token` ADD CONSTRAINT `refresh_token_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
